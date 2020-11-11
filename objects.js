e413bd1a2fc0f1cd061b53441fc2763ad5c6b497
Wendell Silva <dell50cent@gmail.com>
2020-11-11 17:38:23 -0300

    add classes

--- objects.js
+++ objects.js
@@ -1,7 +1,18 @@
-let segurado = {
+let cliente = {
     nome: "João",
     funcao: function() {
-        console.log(this.nome);
+        console.log(this.nome)
     }
 }
-segurado.funcao();  
+class Cliente {
+    constructor(nome, cpf, nascimento){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.nascimento = nascimento;
+    }
+}
+
+let cliente1 = new Cliente("João Augusto", "525.856.952-22", "25/03/1990");
+let cliente2 = new Cliente("João Augusta", "525.856.952-22", "25/03/1990");
+console.log(cliente1);
+console.log(cliente2);

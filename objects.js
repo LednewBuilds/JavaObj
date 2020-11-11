d6f2505c68696a69a6957c637c1afd0421d3d0c3
Wendell Silva <dell50cent@gmail.com>
2020-11-11 18:01:13 -0300

    going home

--- objects.js
+++ objects.js
@@ -16,3 +16,9 @@ let cliente1 = new Cliente("João Augusto", "525.856.952-22", "25/03/1990");
 let cliente2 = new Cliente("João Augusta", "525.856.952-22", "25/03/1990");
 console.log(cliente1);
 console.log(cliente2);
+
+function dec2bin(dec){
+    return (dec >>> 0).toString(2);
+  }
+
+  console.log(dec2bin(1002));

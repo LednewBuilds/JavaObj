d1f68a3a942df90f552985aba6e74d8c17c95010
Wendell Silva <dell50cent@gmail.com>
2020-11-25 10:27:39 -0300

    playable

--- objects.js
+++ objects.js
@@ -2,6 +2,8 @@ document.addEventListener("keydown", Controls, false);
 ////https://stackoverflow.com/questions/48234696/how-to-put-a-gif-with-canvas/48348567
 let puffer;
 let enemies = [];
+let score;
+let tutorial;
 class GameComponent {
     constructor(width, height, color, x, y, type){
         this.type = type;
@@ -43,14 +45,37 @@ class GameComponent {
         if(this.y > bottom){
             this.y = top;
             this.gravitySpeed = 0.05;
-            gameScreen.currentFrame -= 100;
+            if((gameScreen.currentFrame -= 200) < 0){
+                gameScreen.currentFrame = 0;
+            }else{
+            gameScreen.currentFrame -= 200;
+            }
         }
         if(this.y < top){
             this.y = bottom;
             this.gravitySpeed = 0.05;
-            gameScreen.currentFrame -= 100;
+            if((gameScreen.currentFrame -= 200) < 0){
+                gameScreen.currentFrame = 0;
+            }else{
+            gameScreen.currentFrame -= 200;
+            }
         }
         //console.log("This.Y: "+this.y+" - Component Bottom:"+ bottom+" - Component top:"+ this.height);
+    }
+    crash(obj){
+        let myleft = this.x;
+        let myright = this.x + (this.width);
+        let mytop =  this.y;
+        let mybottom = this.y + (this.height);
+        let otherleft = obj.x;
+        let otherright = obj.x + (obj.width);
+        let othertop = obj.y;
+        let otherbottom = obj.y + (obj.height);
+        let crash = true;
+        if((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)){
+            crash = false;
+        }
+        return crash;
     }
 }
 
@@ -58,11 +83,11 @@ const gameScreen = {
     canvas: document.createElement("canvas"),
     start: function(){
         this.ctx = this.canvas.getContext("2d");
-        this.canvas.width = "720";
-        this.canvas.height = "480";
+        this.canvas.width = 720;
+        this.canvas.height = 480;
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.currentFrame = 0;
-        this.interval = setInterval(updateCanvas, 20);    
+        this.interval = setInterval(updateCanvas, 13);    
     },
     clear: function(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -80,6 +105,8 @@ const keyBindings = {
         booster(+0.07);
     }),
     space: (() => {
+        gameScreen.clear();
+        location.reload();
         console.log("space");
     })
 }
@@ -98,12 +125,37 @@ function Controls(e){
 }
 
 function updateCanvas(){
+    let x, height, gap, minHeight, maxHeight, minGap, maxGap;
+    for(i = 0; i < enemies.length; i += 1){
+        if(puffer.crash(enemies[i])){
+            return;
+        }
+    }
     gameScreen.clear();
     gameScreen.currentFrame += 1;
+    console.log(evenNumber(500));
+    if(gameScreen.currentFrame == 1 || evenNumber(160)){
+        x = gameScreen.canvas.width;
+        minHeight  = puffer.height + 100;
+        maxHeight = 120;
+        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+        enemies.push(new GameComponent(10, height, "grey", x, 0));
+        enemies.push(new GameComponent(10, x - height - gap, "grey", x, height + gap));
+    }
+    for(i = 0; i < enemies.length; i += 1){
+        enemies[i].x += -1;
+        enemies[i].update();
+    }
     score.text = "Score: " + gameScreen.currentFrame;
+    tutorial.text = "How to play: [W] [S] [ArrowUp] [ArrowDown] to avoid the objects / [Space] to restart"
     score.update();
+    tutorial.update();
     puffer.updatePosition();
     puffer.update();
+
 }
 
 function booster(n){
@@ -111,13 +163,15 @@ function booster(n){
 }
 
 function evenNumber(n) {
-    ((gameScreen.currentFrame / n) % 1 == 0) ? 'true' : 'false';
+    if((gameScreen.currentFrame / n) % 1 == 0) { return true; }
+    return false;
 }
 
 function startCanvas(){
     puffer = new GameComponent(50, 50, "purple", 60, 120);
     puffer.gravity = 0.05;
-    score = new GameComponent("13px", "Arial","black", 340, 40, "text");
+    score = new GameComponent("16px", "Arial","black", 340, 40, "text");
+    tutorial = new GameComponent("13px", "Calibri", "grey", 20, 460, "text");
     gameScreen.start();
     console.log("Game has been started");
 }
